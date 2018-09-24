5b6118f3141fe712d8e58e49204b8217d4a6d564
natanfudge <natan.lifsiz@gmail.com>
2018-09-24 18:25:32 +0300

    refractor part 3

--- src/main/java/fudge/spatialcrafting/common/crafting/RecipeInputSerialization.java
+++ src/main/java/fudge/spatialcrafting/common/crafting/RecipeInputSerialization.java
@@ -4,8 +4,8 @@ import net.minecraft.nbt.NBTTagCompound;
 
 public final class RecipeInputSerialization {
 
-    public static String RECIPE_INPUT_NBT = "recipeInput";
-    public static String SHAPED_NBT = "shaped";
+    public static final String RECIPE_INPUT_NBT = "recipeInput";
+    public static final String SHAPED_NBT = "shaped";
     private RecipeInputSerialization() {}
 
     static IRecipeInput fromNBT(NBTTagCompound serializedData) {
