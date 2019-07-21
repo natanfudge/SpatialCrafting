fb03c24fc35d626607d5dc34b60bc3b7eeb7c3d8
natanfudge <natan.lifsiz@gmail.com>
2019-07-21 17:24:08 +0300

    commit for test

--- src/main/java/spatialcrafting/ModBlocks.java
+++
@@ -1,9 +0,0 @@
-package spatialcrafting;
-
-import net.minecraftforge.registries.ObjectHolder;
-
-public class ModBlocks {
-
-    @ObjectHolder("spatialcrafting:firstblock")
-    public static FirstBlock FIRSTBLOCK;
-}
--- src/main/java/spatialcrafting/ModItems.java
+++
@@ -1,9 +0,0 @@
-package spatialcrafting;
-
-import net.minecraftforge.registries.ObjectHolder;
-
-public class ModItems {
-
-    @ObjectHolder("spatialcrafting:firstitem")
-    public static FirstItem FIRSTITEM;
-}
